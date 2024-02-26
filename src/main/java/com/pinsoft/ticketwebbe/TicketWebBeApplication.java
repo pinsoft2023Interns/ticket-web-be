2da71f6034e2f50f13e3af44d124510c6497bb55
TugceUsta <tugce.gulsan@gmail.com>
2024-02-26 09:21:47 +0300

    initial commit

---
+++ src/main/java/com/pinsoft/ticketwebbe/TicketWebBeApplication.java
@@ -0,0 +1,13 @@
+package com.pinsoft.ticketwebbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketWebBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketWebBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinsoft/ticketwebbe/TicketWebBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinsoft.ticketwebbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketWebBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
