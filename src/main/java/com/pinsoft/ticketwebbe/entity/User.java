dd29f670af6525911fdb909dbada18a506d490ad
Tuğçe Usta <tugce.gulsan@gmail.com>
2024-03-05 21:37:44 +0300

    control enumType

--- src/main/java/com/pinsoft/ticketwebbe/entity/User.java
+++ src/main/java/com/pinsoft/ticketwebbe/entity/User.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Type;
+import org.hibernate.dialect.PostgreSQLEnumJdbcType;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
