b7dedea03208382a96031be260a32a71d2e93449
Tuğçe Usta <tugce.gulsan@gmail.com>
2024-03-18 10:16:37 +0300

    change jar to war

--- src/main/java/com/pinsoft/ticketwebbe/TicketWebBeApplication.java
+++ src/main/java/com/pinsoft/ticketwebbe/TicketWebBeApplication.java
@@ -2,12 +2,19 @@ package com.pinsoft.ticketwebbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TicketWebBeApplication {
+public class TicketWebBeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TicketWebBeApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TicketWebBeApplication.class);
 	}
 
 }
